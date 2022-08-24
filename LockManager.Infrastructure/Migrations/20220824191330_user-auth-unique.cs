525836457e736a9c398d39298c12a3dcd148ed87
Mario Farias <mfarias@softjournua.onmicrosoft.com>
2022-08-24 16:13:52 -0300

    feat: userauthunique migration

---
+++ LockManager.Infrastructure/Migrations/20220824191330_user-auth-unique.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LockManager.Infrastructure.Migrations
+{
+    public partial class userauthunique : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Username1",
+                table: "UserAuth");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Username1",
+                table: "UserAuth",
+                column: "Username",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Username1",
+                table: "UserAuth");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Username1",
+                table: "UserAuth",
+                column: "Username");
+        }
+    }
+}
