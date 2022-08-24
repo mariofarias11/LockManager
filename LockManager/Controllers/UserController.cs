01d3769f0c8fcbaf0ac9eb753d42b96ff8dacd55
Mario Farias <mfarias@softjournua.onmicrosoft.com>
2022-08-24 19:54:31 -0300

    feat: Authorize user endpoints

--- LockManager/Controllers/UserController.cs
+++ LockManager/Controllers/UserController.cs
@@ -1,6 +1,10 @@
-﻿using LockManager.Domain.Models.Command;
+﻿using System.Security.Claims;
+using LockManager.Domain.Models;
+using LockManager.Domain.Models.Command;
 using LockManager.Domain.Models.Dto;
+using LockManager.Domain.Models.Request;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LockManager.WebApi.Controllers
@@ -16,16 +20,32 @@ namespace LockManager.WebApi.Controllers
             _mediator = mediator;
         }
 
-        [HttpPost]
+        [HttpPost, Authorize]
         public async Task<ActionResult<int>> CreateUser([FromBody] CreateUserCommand command, CancellationToken cancellationToken)
         {
+            var username = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Name).Value;
+            var user = await _mediator.Send(new GetUserQuery { Username = username }, cancellationToken);
+
+            if (user is null || user.Role < Role.Admin || !user.Active)
+            {
+                return Unauthorized();
+            }
+
             var result = await _mediator.Send(command, cancellationToken);
             return Ok(result);
         }
 
-        [HttpPatch, Route("{id:min(1)}")]
+        [HttpPatch, Route("{id:min(1)}"), Authorize]
         public async Task<ActionResult<UserDto>> UpdateUser([FromRoute] int id, [FromBody] UpdateUserCommand command, CancellationToken cancellationToken)
         {
+            var username = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Name).Value;
+            var user = await _mediator.Send(new GetUserQuery { Username = username }, cancellationToken);
+
+            if (user is null || user.Role < Role.Admin || !user.Active)
+            {
+                return Unauthorized();
+            }
+
             command.Id = id;
             var result = await _mediator.Send(command, cancellationToken);
             return Ok(result);
